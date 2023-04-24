a80bb8e05a9ca255547ae2fa2cceae800f89efd8
robertlakatos <lrobert986@gmail.com>
2023-04-24 17:43:37 +0200

    bayes

--- search.py
+++ search.py
@@ -1,5 +1,6 @@
 import numpy as np
-from libs.node import Node
+from node import Node
+from collections import deque
 
 def trial_error(problem):
     state = Node(problem.initial)
@@ -33,3 +34,30 @@ def hill_climbing_for_3Cup(problem, heuristic):
             return 'Unsolvable'
 
         state=test_succesors[np.random.randint(0,len(test_succesors))]
+
+def breadth_first_tree_search(problem):
+
+    frontier = deque([Node(problem.initial)])
+
+    while frontier:
+        node = frontier.popleft()
+
+        if problem.goal_test(node.state):
+            return node
+
+        frontier.extend(node.expand(problem))
+
+def depth_first_graph_search(problem):
+    frontier = [(Node(problem.initial))] 
+    explored = set()
+
+    while frontier:
+        node = frontier.pop()
+
+        if problem.goal_test(node.state):
+            return node
+
+        explored.add(node.state)
+
+        frontier.extend(child for child in node.expand(problem)
+                        if child.state not in explored and child not in frontier)
